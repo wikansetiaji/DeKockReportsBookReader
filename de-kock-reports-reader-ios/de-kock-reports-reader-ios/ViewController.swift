4871d7f1e385b58b6b317d70c98f126dc8cf129e
Wikan Setiaji <wikansetiaji@gmail.com>
2025-07-15 15:16:03 +0700

    Add: xcode project and fonts and window size logic

---
+++ de-kock-reports-reader-ios/de-kock-reports-reader-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  de-kock-reports-reader-ios
+//
+//  Created by Wikan Setiaji on 15/07/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,20 @@
+import 'dart:io';
+
 import 'package:de_kock_reports_reader/pages/simple_book_reader_page.dart';
 import 'package:de_kock_reports_reader/service/size_config.dart';
 import 'package:flutter/material.dart';
+import 'package:window_size/window_size.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (Platform.isWindows || Platform.isMacOS) {
+    setWindowTitle('Your App');
+    setWindowMinSize(const Size(1133, 744));
+    setWindowMaxSize(Size.infinite);
+    setWindowFrame(const Rect.fromLTWH(100, 100, 1133, 744)); // 16:9
+  }
+
   runApp(const MyApp());
 }
 
